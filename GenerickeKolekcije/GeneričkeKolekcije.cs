4031ca958e60c9838c893c0da2a4e2c07056208d
Anton Kolnrekaj <toni1korni@gmail.com>
2016-12-15 21:03:34 +0100

    Riješili smo GeneričkeKolekcije

--- GenerickeKolekcije/GeneričkeKolekcije.cs
+++ GenerickeKolekcije/GeneričkeKolekcije.cs
@@ -10,13 +10,16 @@ namespace Vsite.CSharp
         {
             ArrayList osobe = new ArrayList();
             // TODO: U listu 'osobe' dodati dvije osobe s imenima "Ivica" i "Marica" te proizvoljnim datumima rođenja. Prevesti program, pokrenuti ga i provjeriti ispis.
-
+            Osoba Ivica = new Osoba("Ivica", new DateTime(1873, 11, 2));
+            Osoba Marica = new Osoba("Marica", new DateTime(1875, 11, 2));
+            osobe.Add(Ivica);
+            osobe.Add(Marica);
 
             foreach (Osoba osoba in osobe)
                 Console.WriteLine(osoba.Ime);
 
             // TODO: U listu 'osobe' dodati objekt nekog drugog tipa. Prevesti program, pokrenuti ga i provjeriti ispis.
-
+            osobe.Add(3);
             foreach (Osoba osoba in osobe)
                 Console.WriteLine(osoba.Ime);
         }
@@ -24,8 +27,19 @@ namespace Vsite.CSharp
         public static void GeneričkaKolekcija()
         {
             List<Osoba> osobe = new List<Osoba>();
-            // TODO: Ponoviti dodavananja i ispise iz gornje metode. Prevesti program, pokrenuti ga i provjeriti ispis.
+            //  Ponoviti dodavananja i ispise iz gornje metode. Prevesti program, pokrenuti ga i provjeriti ispis.
+            Osoba Ivica = new Osoba("Ivica", new DateTime(1873, 11, 2));
+            Osoba Marica = new Osoba("Marica", new DateTime(1875, 11, 2));
+            osobe.Add(Ivica);
+            osobe.Add(Marica);
 
+            foreach (Osoba osoba in osobe)
+                Console.WriteLine(osoba.Ime);
+
+            //  U listu 'osobe' dodati objekt nekog drugog tipa. Prevesti program, pokrenuti ga i provjeriti ispis.
+            //osobe.Add(3);
+            foreach (Osoba osoba in osobe)
+                Console.WriteLine(osoba.Ime);
         }
 
         static void Main(string[] args)
